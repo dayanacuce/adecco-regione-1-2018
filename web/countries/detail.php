6cbb91edf8dd81f272a8347b03a1cb03ba76550d
Unknown <lockit@lockit.it>
2018-05-09 17:54:59 +0200

    details

--- web/countries/detail.php
+++ web/countries/detail.php
@@ -56,54 +56,52 @@
             </div>
             <!-- /.navbar-static-side -->
         </nav>
+        <?php
+        include '../php_utils/db_utils.php';
+
+        $db_utils = new DbUtils();
+        $conn = $db_utils->getConnection();
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE alpha2code='".$_GET['alpha2code']."'");
+        $stmt->execute();
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $country = $stmt->fetch();
+          ?>
 
         <!-- Page Content -->
         <div id="page-wrapper">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header">Contact form</h1>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                Country list
-                            </div>
-                            <!-- /.panel-heading -->
-                            <div class="panel-body">
-                                <div class="table-responsive">
-                                    <table class="table table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th>alpha2Code</th>
-                                                <th>Name</th>
-                                                <th>Capital</th>
-                                                <th>commands</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                          <?php
-                                          include '../php_utils/db_utils.php';
+                        <h1 class="page-header"><?php echo $country['name'] ?></h1>
 
-                                          $db_utils = new DbUtils();
-                                          $conn = $db_utils->getConnection();
-                                          $stmt = $conn->prepare("SELECT alpha2code, name, capital FROM countries");
-                                          $stmt->execute();
-
-                                                  $commands = '<td><a href="detail.php?alpha2code=:alpha2code">dettaglio</a></td>';
-
-                                          // set the resulting array to associative
-                                          $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                                          foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll()), '', $commands, 'alpha2code') as $k=>$v) {
-                                              echo $v;
-                                          }
-                                          ?>
-                                        </tbody>
-                                    </table>
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-user fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <div class="huge"><?php echo $country['population']; ?></div>
+                                                <div>Population</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                  </div>
                                 </div>
-                                <!-- /.table-responsive -->
-                            </div>
-                            <!-- /.panel-body -->
-                        </div>
-                        <!-- /.panel -->
+                                <div class="col-lg-3 col-md-6">
+                                  <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                      Capital: <?php echo $country['capital']; ?>
+                                    </div>
+                                    <div class="panel-body">
+                                      <img src="<?php echo $country['flag']; ?>" style="width: 100%;"/>
+                                    </div>
+                                </div>
+                                </div>
+                              </div>
+                        <br /><br />
                     </div>
                     <!-- /.col-lg-12 -->
                 </div>
