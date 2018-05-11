27ba7c9d079bc751ef653f42c82113ccab2b5c2d
Unknown <piermarco90@tiscali.it>
2018-05-11 11:26:35 +0200

    a

--- web/football/details_piermarco.php
+++ web/football/details_piermarco.php
@@ -1,97 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include 'php_utils/db_utils.php';
 
-<head>
+$db_utils = new DbUtils();
+$conn = $db_utils->getConnection();
+$stmt = $conn->prepare("SELECT * FROM countries WHERE alpha2code='".$_GET['alpha2code']."'");
+$stmt->execute();
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+$country = $stmt->fetch();
+  ?>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<div class="row">
+<div class="col-lg-12">
+    <h1 class="page-header"><?php echo $country['name'] ?></h1>
 
-    <title>Informazioni Partita</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/bootstrap-datepicker.standalone.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="../index.html">SB Admin v2.0</a>
-            </div>
-            <!-- /.navbar-header -->
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li>
-                            <a href="../index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
-
-        <!-- Page Content -->
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Partita Juve - Milan</h1>
-
-
-                        <br /><br />
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-user fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo $country['population']; ?></div>
+                            <div>Population</div>
+                        </div>
                     </div>
-                    <!-- /.col-lg-12 -->
                 </div>
-                <!-- /.row -->
+              </div>
             </div>
-            <!-- /.container-fluid -->
-        </div>
-        <!-- /#page-wrapper -->
-
-    </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-    <script src="../js/bootstrap-datepicker.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
-
-    <script src="../js/contact_form.js"></script>
-</body>
-
-</html>
+            <div class="col-lg-3 col-md-6">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  Capital: <?php echo $country['capital']; ?>
+                </div>
+                <div class="panel-body">
+                  <img src="<?php echo $country['flag']; ?>" style="width: 100%;"/>
+                </div>
+            </div>
+            </div>
+          </div>
+    <br /><br />
+</div>
+<!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
