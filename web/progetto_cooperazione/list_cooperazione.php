c485947663f39aa67dbe412c1a7e508725f6fc72
Unknown <dayanacuce@gmail.com>
2018-05-11 13:09:15 +0200

    all

--- web/progetto_cooperazione/list_cooperazione.php
+++ web/progetto_cooperazione/list_cooperazione.php
@@ -4,10 +4,8 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">Contact form</h1>
-
-
                           <div class="row">
-                              <div class="col-lg-6">
+                              <div class="col-lg-12">
                                   <div class="panel panel-default">
                                       <div class="panel-heading">
                                           Progetto cooperazione
@@ -15,7 +13,8 @@
                                       <!-- /.panel-heading -->
                                       <div class="panel-body">
                                           <div class="table-responsive">
-                                              <table class="table table-striped table-bordered table-hover">
+                                              <table class="table table-striped table-bordered table-hover"
+                                                     width="100%">
                                                   <thead>
                                                       <tr>
                                                           <th>#</th>
